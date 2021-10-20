ade1b84bc614ce6235e1232457c4c10892fa8aa7
onionmccabbage <onionmccabbage@live.com>
2021-10-20 16:30:00 +0100

    Add files via upload

---
+++ ES_code/script.js
@@ -0,0 +1,8 @@
+// this script is re-useable across multiple pages, and is also easier to test
+// we can access the HTML elements
+let output = document.getElementById('monitor')
+output.innerHTML = 'Welcome'
+// Objects - in JavaScript EVERYTHING is an Object
+let obj = {item:'Pot', price:3.99, qty:6, onsale:false}
+// we can see that all Object derive from the prototype
+console.log(obj)
