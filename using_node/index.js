e3060d9d7cbd7b8f25df00de556af188875090e3
onionmccabbage <onionmccabbage@live.com>
2021-10-22 16:07:26 +0100

    Add files via upload

--- using_node/index.js
+++ using_node/index.js
@@ -1,3 +1,8 @@
 // to use npm we MUST have installed NodeJS
 // npm is the Node Package Manager
 // npx is also NPM but with eXecution capabilities - run immediatly!
+
+// we use 'npm install jquery -save' to download the latest
+// jquery and save it's dependency in package.json
+// then if we ever need to re-install dependencies, we use
+// npm install
